b45e71f24f830da03aaec0fc88f680525b424f4c
Walt Disney <wyattreiman@wyatts-mbp-2.lan>
2019-01-09 15:36:13 -0600

    not even close

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,25 @@
+var express = require("express");
+
+var router = express.Router();
+
+var cat = require("../models/burger.js");
+
+var burger = require("../models/burger.js");
+
+
+router.get("/", function(req,res){
+    burger.all(function(burger_data){
+        console.log(burger_data);
+        res.render("index", {burger_data});
+    });
+});
+
+router.put("/burgers/update", function(req, res){
+    burger.update(req.body.burger_id, function(result){
+        console.log(result);
+        res.redirect("/");
+    });
+});
+
+
+module.exports = router;
